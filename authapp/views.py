ba122fbe1ba326f59fc1a4b8cd37d091d6cc15d5
Andrey Belyaev <ianbel263@gmail.com>
2021-11-22 10:04:37 +0300

    add authapp templates

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def register(request):
+    context = {
+        'title': 'GeekShop - Регистрация'
+    }
+    return render(request, 'authapp/register.html', context)
+
+
+def login(request):
+    context = {
+        'title': 'GeekShop - Авторизация'
+    }
+    return render(request, 'authapp/login.html', context)
