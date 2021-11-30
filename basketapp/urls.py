0b20213cb85afe32d9d1ee5eef6865a1eeeae4a4
Andrey Belyaev <ianbel263@gmail.com>
2021-11-30 20:33:35 +0300

    add ajax

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from basketapp.views import add_basket, delete_basket
+from basketapp.views import add_basket, delete_basket, update_basket
 
 app_name = 'basketapp'
 urlpatterns = [
-    path('add/<int:product_id>', add_basket, name='add_basket'),
-    path('delete/<int:basket_id>', delete_basket, name='delete_basket')
+    path('add/<int:product_id>/', add_basket, name='add_basket'),
+    path('delete/<int:basket_id>/', delete_basket, name='delete_basket'),
+    path('update/', update_basket, name='update_basket')
 ]
--- basketapp/views.py
+++ basketapp/views.py
@@ -1,10 +1,14 @@
 # Create your views here.
-from django.http import HttpResponseRedirect
+import json
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, JsonResponse
 
 from basketapp.models import Basket
 from mainapp.models import Product
 
 
+@login_required
 def add_basket(request, product_id):
     user = request.user
     product = Product.objects.get(id=product_id)
@@ -15,9 +19,30 @@ def add_basket(request, product_id):
         basket.save()
     else:
         Basket.objects.create(user=user, product=product, quantity=1)
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    return JsonResponse({
+        'success': True
+    })
 
 
+@login_required
 def delete_basket(request, basket_id):
     Basket.objects.get(id=basket_id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def update_basket(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        basket_id = data['basketID']
+        quantity = int(data['quantity'])
+        basket = Basket.objects.get(id=basket_id)
+        basket.quantity = quantity
+        basket.save()
+        return JsonResponse({
+            'basketID': basket_id,
+            'quantity': quantity,
+            'price': basket.get_sum(),
+            'totalPrice': basket.get_total_sum(),
+            'totalQuantity': basket.get_total_quantity()
+        })
