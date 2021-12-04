60765b44631ddb67eb9a6b091269750999630cd9
Andrey Belyaev <ianbel263@gmail.com>
2021-12-04 08:53:49 +0300

    update models

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, unique=True)
     desc = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -16,6 +19,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to='product_img', blank=True)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.name} | {self.category}'
