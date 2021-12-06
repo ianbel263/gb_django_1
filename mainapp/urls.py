1e260df74d6ea87108fb6f6def0cf49970efdf85
Andrey Belyaev <ianbel263@gmail.com>
2021-12-06 17:48:17 +0300

    update mainapp views to CBV

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from mainapp.views import products, detail
+from mainapp.views import ProductsListView, ProductDetailView
 
 app_name = 'mainapp'
 urlpatterns = [
-    path('<int:category_pk>', products, name='products'),
-    path('<int:pk>/', detail, name='detail'),
+    path('', ProductsListView.as_view(), name='products'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='detail'),
+    path('category/<int:pk>', ProductsListView.as_view(), name='category'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,41 +1,33 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.views.generic import TemplateView, ListView, DetailView
 
-from .models import Category, Product
-
-
-# Create your views here.
-def index(request):
-    title = 'GeekShop'
-    context = {
-        'title': title
-    }
-    return render(request, 'mainapp/index.html', context)
+from .models import Product, Category
 
 
-def products(request, category_pk):
-    title = 'GeekShop - Каталог'
-    if category_pk == 0:
-        category = 'Все товары'
-        selected_products = Product.objects.filter(is_active=True)
-    else:
-        category = get_object_or_404(Category, pk=category_pk)
-        selected_products = Product.objects.filter(category=category_pk, is_active=True)
-
-    context = {
-        'title': title,
-        'category': category,
-        'categories': Category.objects.filter(is_active=True),
-        'products': selected_products
-    }
-    return render(request, 'mainapp/products.html', context)
+class IndexTemplateView(TemplateView):
+    template_name = 'mainapp/index.html'
+    extra_context = {'title': 'GeekShop'}
 
 
-def detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    title = f'GeekShop - {product.name}'
-    context = {
-        'title': title,
-        'product': product
-    }
-    return render(request, 'mainapp/detail.html', context)
+class ProductsListView(ListView):
+    queryset = Product.objects.filter(is_active=True)
+    template_name = 'mainapp/products.html'
+    extra_context = {'title': 'GeekShop - Каталог'}
+    context_object_name = 'products'
 
+    def get_context_data(self, **kwargs):
+        self.extra_context.update({'categories': Category.objects.filter(is_active=True)})
+        return super(ProductsListView, self).get_context_data(**kwargs)
+
+    def get_queryset(self):
+        category_pk = self.kwargs.get('pk')
+        return Product.objects.filter(is_active=True, category=self.kwargs.get('pk')) if category_pk else self.queryset
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'mainapp/detail.html'
+    extra_context = {}
+
+    def get_context_data(self, **kwargs):
+        self.extra_context.update({'title': f'GeekShop - {self.object.name}'})
+        return super(ProductDetailView, self).get_context_data(**kwargs)
