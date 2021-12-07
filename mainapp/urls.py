317d38aba299d9f6b1358808770fed79849ad61a
Andrey Belyaev <ianbel263@gmail.com>
2021-12-07 08:44:33 +0300

    fix bugs

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', ProductsListView.as_view(), name='products'),
     path('category/<int:category_pk>', ProductsListView.as_view(), name='category'),
     path('page/<int:page_number>', ProductsListView.as_view(), name='page'),
+    path('category/<int:category_pk>/page/<int:page_number>', ProductsListView.as_view(), name='category_by_page'),
     path('<int:pk>/', ProductDetailView.as_view(), name='detail'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.core.paginator import Paginator
 from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import Product, Category
@@ -10,20 +10,36 @@ class IndexTemplateView(TemplateView):
 
 
 class ProductsListView(ListView):
+    model = Product
     queryset = Product.objects.filter(is_active=True)
+    category_pk = None
     template_name = 'mainapp/products.html'
-    extra_context = {'title': 'GeekShop - Каталог'}
     context_object_name = 'products'
-    paginate_by = 3
+    paginate_by = 6
     page_kwarg = 'page_number'
 
     def get_context_data(self, **kwargs):
-        self.extra_context.update({'categories': Category.objects.filter(is_active=True)})
-        return super(ProductsListView, self).get_context_data(**kwargs)
+        self.category_pk = self.kwargs.get('category_pk')
+        title = 'GeekShop - Каталог'
+        if self.category_pk:
+            self.queryset = Product.objects.filter(is_active=True, category=self.category_pk)
+            title = f'GeekShop - Каталог|{Category.objects.get(pk=self.category_pk).name}'
 
-    def get_queryset(self):
-        category_pk = self.kwargs.get('category_pk')
-        return Product.objects.filter(is_active=True, category=category_pk) if category_pk else self.queryset
+        context = super(ProductsListView, self).get_context_data(object_list=self.queryset, **kwargs)
+        if self.paginate_by:
+            current_page = context['page_obj'].number
+            page_range = context['paginator'].get_elided_page_range(current_page)
+        else:
+            current_page = None
+            page_range = None
+
+        context.update({
+            'title': title,
+            'current_category': self.category_pk,
+            'categories': Category.objects.filter(is_active=True),
+            'page_range': page_range
+        })
+        return context
 
 
 class ProductDetailView(DetailView):
