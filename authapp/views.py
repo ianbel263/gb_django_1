c72fca91104c0741f4cdc2cf58c428a3311ea59c
Andrey Belyaev <ianbel263@gmail.com>
2021-12-13 17:33:27 +0300

    add email verification

--- authapp/views.py
+++ authapp/views.py
@@ -1,6 +1,11 @@
+from django.conf import settings
+from django.contrib import auth
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LogoutView, LoginView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 from django.views.generic import CreateView, UpdateView
@@ -44,6 +49,34 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     extra_context = {'title': 'GeekShop - Регистрация'}
     success_url = reverse_lazy('authapp:login')
     success_message = success_messages['register']
+
+    def form_valid(self, form):
+        response = super(UserCreateView, self).form_valid(form)
+        try:
+            self.send_verify_link(self.object)
+        except Exception as e:
+            pass
+        return response
+
+    @staticmethod
+    def send_verify_link(user):
+        verify_link = reverse_lazy('authapp:verify', args=[user.email, user.activation_key])
+        subject = f'Для активации учетной записи {user.username} пройдите по ссылке'
+        message = f'Для подтверждения учетной записи {user.username} на портале {settings.DOMAIN_NAME}\nпройдите по ссылке: {settings.DOMAIN_NAME}{verify_link}'
+        return send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+    def verify(self, email, activation_key):
+        try:
+            user = User.objects.get(email=email)
+            if user and user.activation_key == activation_key and not user.is_activation_key_expired():
+                user.activation_key = ''
+                user.activation_key_expires_at = None
+                user.is_active = True
+                user.save()
+                auth.login(self, user)
+            return render(self, 'authapp/verification.html')
+        except Exception as e:
+            return HttpResponseRedirect(reverse_lazy('index'))
 
 
 class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +136,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authapp.User'
 LOGIN_URL = 'authapp:login'
 LOGIN_REDIRECT_URL = 'index'
+
+load_dotenv(BASE_DIR / '.env')
+DOMAIN_NAME = 'http://localhost:8000'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = 'tmp/emails'
