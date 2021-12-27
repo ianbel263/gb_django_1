0fd12f6f029e33782f3097af8ba6b7e1bde1c981
Andrey Belyaev <ianbel263@gmail.com>
2021-12-27 15:00:26 +0300

    debug -> True

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1$37z%k_hxr3u-!kl&^(duv7#uan=tmw4r&c)9m-00o0@zl&_h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
