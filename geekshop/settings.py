475a215c69ab2d1524b7265279a40522a09d1499
Andrey Belyaev <ianbel263@gmail.com>
2022-01-04 22:55:24 +0300

    change email settings

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -151,8 +151,8 @@ LOGIN_REDIRECT_URL = 'index'
 LOGIN_ERROR_URL = 'index'
 
 DOMAIN_NAME = 'ianbel.ru'
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
