d4eb12cf53cdeadf546b673eb2fc208086a7a481
Andrey Belyaev <ianbel263@gmail.com>
2022-01-07 11:40:17 +0300

    remove unneeded

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker exec geekshop_web python3 manage.py migrate --noinput
-docker exec geekshop_web python3 manage.py collectstatic --noinput
-docker exec geekshop_web python3 manage.py createsuperuser --noinput --email 1@1.ru
-docker exec geekshop_web python3 manage.py loaddata ./fixtures/categories.json
-docker exec geekshop_web python3 manage.py loaddata ./fixtures/products.json
